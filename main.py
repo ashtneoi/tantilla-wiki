25dea3561a6e3a3b4269dbaa6576d3abd75a7678
ashtneoi <ashtneoi@gmail.com>
2018-04-13 00:51:16 -0700

    Make autosave better
    Previously simplemde would clear the autosaved text on submit, even if
    the POST failed. Now it immediately autosaves on submit and only clears
    the autosaved text when the next GET confirms the POST was successful.
    
    simplemde files are from
    https://github.com/ashtneoi/simplemde-markdown-editor at revision
    b275b521d879701d68ba2dbef46a48b97179c3f4.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from random import randrange
+
 from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
+from werkzeug.wrappers import Response
 
 from bakery import render_path
 from config import config
@@ -7,18 +11,39 @@ from tantilla import create_app, HTMLResponse
 
 MOUNT_POINT = config["mount_point"]
 
+stamp = randrange(0, 1<<31)
+stamp_mask = (1<<32) - 1
+prev_stamp = 0
+
 
 def page(req, name):
     if req.method == 'POST':
+        if 'stamp' not in req.form or 'text' not in req.form:
+            return abort(400)
+        global prev_stamp
+        try:
+            prev_stamp = int(req.form["stamp"])
+            print(req.form["text"])
+        except ValueError:
+            return abort(400)
+        return redirect(req.full_path, code=303)
+    if 'edit' in req.args:
+        global stamp
+        resp = HTMLResponse(
+            render_path('tmpl/page.htmo', {
+                'base': MOUNT_POINT,
+                'content': "HOI!",
+                'stamp': str(stamp),
+                'prevstamp': str(prev_stamp),
+                'title': "Edit page",
+            })
+        )
+        stamp = (stamp + 1) & stamp_mask
+        return resp
+    else:
         return abort(400)
-    return HTMLResponse(
-        render_path("tmpl/page.htmo", {
-            "base": MOUNT_POINT,
-            "title": "Edit page",
-        })
-    )
 
 
 application = create_app(MOUNT_POINT, (
-    ("<path:name>", page),
+    ('<path:name>', page),
 ))
