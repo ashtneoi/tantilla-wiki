2455b2273b1075f073e92c6e20ad324341316068
ashtneoi <ashtneoi@gmail.com>
2018-04-13 02:06:17 -0700

    Use real files; use status instead of abort

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from os import makedirs, path
 from random import randrange
 
-from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
 from werkzeug.wrappers import Response
 
 from bakery import render_path
 from config import config
-from tantilla import create_app, HTMLResponse
+from tantilla import create_app, HTMLResponse, status
 
 
 MOUNT_POINT = config["mount_point"]
@@ -17,22 +17,36 @@ prev_stamp = 0
 
 
 def page(req, name):
+    assert not (name.startswith("./") or name.startswith("../"))
+    if name.startswith(".") or name.endswith("/"):
+        return status(req, 400)
+
     if req.method == 'POST':
         if 'stamp' not in req.form or 'text' not in req.form:
-            return abort(400)
+            return status(req, 400)
         global prev_stamp
         try:
-            prev_stamp = int(req.form["stamp"])
-            print(req.form["text"])
+            prev_stamp = int(req.form['stamp'])
         except ValueError:
-            return abort(400)
+            return status(req, 400)
+        filename = "repo/" + name
+        makedirs(path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as f:
+            f.write(req.form['text'] + "\n")
         return redirect(req.full_path, code=303)
+
     if 'edit' in req.args:
+        filename = "repo/" + name
+        try:
+            with open(filename, "r") as f:
+                content = f.read().rstrip("\n")
+        except FileNotFoundError:
+            return status(req, 404)
         global stamp
         resp = HTMLResponse(
             render_path('tmpl/page.htmo', {
                 'base': MOUNT_POINT,
-                'content': "HOI!",
+                'content': content,
                 'stamp': str(stamp),
                 'prevstamp': str(prev_stamp),
                 'title': "Edit page",
@@ -41,7 +55,7 @@ def page(req, name):
         stamp = (stamp + 1) & stamp_mask
         return resp
     else:
-        return abort(400)
+        return status(req, 400)
 
 
 application = create_app(MOUNT_POINT, (
